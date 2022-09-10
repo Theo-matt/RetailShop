571bda98abb76ebd99570e19fc2bb746bd7d4521
the-soft <thesoft.oncode@gmail.com>
2022-09-10 20:46:10 +0100

    Add WPF Project with MVVM
    Adds our WPF user interface project and configures it with caliburn micro for our MVVM framework. Also include the Simple Container DI system.

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        private readonly SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            LogManager.GetLog = type => new DebugLog(type);
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            foreach (var assembly in SelectAssemblies())
+            {
+                assembly.GetTypes()
+                        .Where(type => type.IsClass)
+                        .Where(type => type.Name.EndsWith("ViewModel"))
+                        .ToList()
+                        .ForEach(viewModelType => _container.RegisterPerRequest(
+                            viewModelType, viewModelType.ToString(), viewModelType));
+            }
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await DisplayRootViewForAsync(typeof(ShellViewModel));
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
